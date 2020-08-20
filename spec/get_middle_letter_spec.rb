4ef0a48063cc7ead1ad7931242b6f950a59c64ae
RichEwin <richardewinlambert@gmail.com>
2020-08-20 16:01:57 +0100

    RED-returns a word with two letters

--- spec/get_middle_letter_spec.rb
+++ spec/get_middle_letter_spec.rb
@@ -4,4 +4,8 @@ describe ('#get_middle') do
     it 'returns single letter' do 
         expect(get_middle('A')).to eq('A')
     end 
+
+    it 'returns a word with two letters' do 
+        expect(get_middle('Of')).to eq('Of')
+    end 
 end   
