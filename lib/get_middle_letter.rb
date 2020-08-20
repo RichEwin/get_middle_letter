34550e9f4532cd078fdafe9a94c99197666f0963
RichEwin <richardewinlambert@gmail.com>
2020-08-20 16:13:10 +0100

    REFACTORED

--- lib/get_middle_letter.rb
+++ lib/get_middle_letter.rb
@@ -1,8 +1,8 @@
 def get_middle(word)
     if word.length.odd?
-        return word[word.length/2]
+        word[word.length/2]
     else
-        return word[word.length/2-1..word.length/2]
+        word[word.length/2-1..word.length/2]
     end 
 end 
 
